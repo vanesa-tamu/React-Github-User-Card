233f7cc75337e7a71bc0baa5c06d764a84f91bc1
vanesa-tamu <vahe5495@tamu.edu>
2020-03-10 16:43:28 -0500

    commit one

---
+++ github/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      github: []
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        Hello! 
+      </div>
+    )
+  }
+}
+
+export default App;
